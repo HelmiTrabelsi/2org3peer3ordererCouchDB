e340cb39821189f3a399045f67bb054cbaacbaee
HelmiTrabelsi <helmi.trabelsi102@gmail.com>
2021-05-06 08:23:34 -0400

    update_block_size4

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,5 @@
 ./createChannelConf.sh mycc
 sudo git add *
 sudo git commit -m "update_block_size4"
-git pull --rebase https://github.com/HelmiTrabelsi/2peer3ordererCouchDB.git master
-git push https://github.com/HelmiTrabelsi/2peer3ordererCouchDB.git master
+git pull --rebase https://github.com/HelmiTrabelsi/2org3peer3ordererCouchDB.git master
+git push https://github.com/HelmiTrabelsi/2org3peer3ordererCouchDB.git master
